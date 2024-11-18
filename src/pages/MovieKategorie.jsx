b89490f45217ed9385673f415997e79ea4059283
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-19 08:40:28 +0900

    [feat] 임시 카테고리 컴포넌트 구현 #6

---
+++ src/pages/MovieKategorie.jsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import postApi from "../api/postsApi";
+
+export default function MovieKategorie() {
+  const navigate = useNavigate();
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchNowPlay() {
+      try {
+        const data = await postApi.getNowPlay();
+
+        setNowPlaying(data);
+      } catch (err) {
+        setError(err.message);
+
+        console.error(err);
+        console.log("에러남 ㅠㅠ");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchNowPlay();
+  }, []);
+
+  if (loading) {
+    return <div>로딩중</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div>
+      <h2>NowPlay</h2>
+      <ul>
+        {nowPlaying.map((nowPlay) => {
+          const { id, title, poster_path } = nowPlay;
+          return (
+            <li key={id}>
+              {/* <Link to={`/posts/${id}`} state={{ nowPlay }}>
+                <h3>{title}</h3>
+              </Link>
+              <h3
+                onClick={() => {
+                  navigate(`/posts/${id}`);
+                }}
+              >
+                {title}
+              </h3> */}
+              <img src={poster_path} />
+              <h4>{title}</h4>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
