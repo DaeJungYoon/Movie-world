cfe0e09fa4136ce4a1cae2b18155fa864ac083d4
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-19 08:41:44 +0900

    [feat] 임시 카테고리 컴포넌트 메인페이지에 렌더링 #7

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,18 +1,11 @@
 import React from "react";
+import MovieKategorie from "./MovieKategorie";
 
 export default function MainPage() {
   return (
     <>
       <h1>Movie World</h1>
-      <ul>
-        <li>
-          <h3>현재 상영중</h3>
-        </li>
-        <li>
-          <img src="./preImg.png"></img>
-          <p>제목</p>
-        </li>
-      </ul>
+      <MovieKategorie></MovieKategorie>
       <ul>
         <li>
           <h3>인기있는 영화</h3>
