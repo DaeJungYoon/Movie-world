a80df092cd9cfc455de47ca2698f81164cebff84
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-19 19:07:51 +0900

    [feat] MovieList 컴포넌트 기능 구현 #22

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function MovieKategorie(props) {
+  const { kategorieTitle, state } = props;
+  return (
+    <div>
+      <h2>{kategorieTitle}</h2>
+      <p>...more</p>
+      <ul>
+        {state.map((item) => {
+          const { id, title, poster_path } = item;
+          return (
+            <li key={id}>
+              <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} />
+              <h4>{title}</h4>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
