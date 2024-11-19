39420e8328faaba67de0c6d3590d640f6e2106a2
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-19 15:25:06 +0900

    [refactor,feat] nowplay 리팩토링, popular&&topRated 기능 구현 #13

--- src/pages/MovieKategorie.jsx
+++ src/pages/MovieKategorie.jsx
@@ -2,11 +2,15 @@ import React, { useEffect, useState } from "react";
 // import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 // import axios from "axios";
-import nowPlayingApi from "../api/nowPlayingApi";
+// import nowPlayingApi from "../api/nowPlayingApi";
+import { nowPlayingApi, popularApi, topRatedApi } from "../api/movieApi";
+import { combineSlices } from "@reduxjs/toolkit";
 
 export default function MovieKategorie() {
   // const navigate = useNavigate();
   const [nowPlaying, setNowPlaying] = useState([]);
+  const [pupulars, setPupulars] = useState([]);
+  const [topRateds, setTopRateds] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
@@ -14,7 +18,6 @@ export default function MovieKategorie() {
     async function fetchNowPlay() {
       try {
         const data = await nowPlayingApi.getNowPlaying();
-
         setNowPlaying(data);
       } catch (err) {
         setError(err.message);
@@ -27,6 +30,40 @@ export default function MovieKategorie() {
     }
 
     fetchNowPlay();
+  }, []);
+  useEffect(() => {
+    async function fetchPopular() {
+      try {
+        const data = await popularApi.getPopular();
+        setPupulars(data);
+      } catch (err) {
+        setError(err.message);
+
+        console.error(err);
+        console.log("에러남 ㅠㅠ");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchPopular();
+  }, []);
+  useEffect(() => {
+    async function fetchTopRated() {
+      try {
+        const data = await topRatedApi.getTopRated();
+        setTopRateds(data);
+      } catch (err) {
+        setError(err.message);
+
+        console.error(err);
+        console.log("에러남 ㅠㅠ");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchTopRated();
   }, []);
 
   if (loading) {
@@ -45,17 +82,31 @@ export default function MovieKategorie() {
           const { id, title, poster_path } = nowPlay;
           return (
             <li key={id}>
-              {/* <Link to={`/posts/${id}`} state={{ nowPlay }}>
-                <h3>{title}</h3>
-              </Link>
-              <h3
-                onClick={() => {
-                  navigate(`/posts/${id}`);
-                }}
-              >
-                {title}
-              </h3> */}
-              <img src={poster_path} />
+              <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} />
+              <h4>{title}</h4>
+            </li>
+          );
+        })}
+      </ul>
+      <h2>Popular</h2>
+      <ul>
+        {pupulars.map((pupular) => {
+          const { id, title, poster_path } = pupular;
+          return (
+            <li key={id}>
+              <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} />
+              <h4>{title}</h4>
+            </li>
+          );
+        })}
+      </ul>
+      <h2>Top Rated</h2>
+      <ul>
+        {topRateds.map((topRated) => {
+          const { id, title, poster_path } = topRated;
+          return (
+            <li key={id}>
+              <img src={`https://image.tmdb.org/t/p/w200${poster_path}`} />
               <h4>{title}</h4>
             </li>
           );
