3f8a19d79f8a4078f1db2f42d7ae874fa6b45b37
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-19 17:28:52 +0900

    [refactor] mainPage 임시 재구성 #17

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import MovieKategorie from "./MovieKategorie";
+import { Link } from "react-router-dom";
 
 export default function MainPage() {
   return (
     <>
       <h1>Movie World</h1>
-      <MovieKategorie></MovieKategorie>
-
+      <button>Login</button>
+      <Link to="/kategorie">Movie ON!</Link>
     </>
   );
 }
