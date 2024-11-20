8714af4aa447144916e4049f57f374e335359636
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-20 15:42:49 +0900

    [refactor] MovieCard컴포넌트에서 받아온 props 명 items에서 item으로 변경  MovieCard.jsx 리펙토링 #31

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function MovieCard(props) {
   // console.log(props);
-  const { items } = props;
+  const { item } = props;
 
   return (
-    <li key={items.id}>
-      <img src={`https://image.tmdb.org/t/p/w200${items.poster_path}`} />
-      <h4>{items.title}</h4>
+    <li key={item.id}>
+      <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} />
+      <h4>{item.title}</h4>
     </li>
   );
 }
