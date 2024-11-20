5e380528efbae6bc6df8f5a1c30d3d36d44f9543
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-20 15:56:44 +0900

    [feat] PopularMore페이지 구현  PopularMore.jsx  #36

--- src/pages/PopularMore.jsx
+++ src/pages/PopularMore.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React from "react";
+import { popularApi } from "../api/movieApi";
+import { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard";
 
 export default function PopularMore() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovieData() {
+      try {
+        const popdata = await popularApi.getPopular();
+
+        const newArr = [
+          {
+            kategorieTitle: "popular_more",
+            movies: popdata,
+          },
+        ];
+
+        // 렌더링 할 데이터 상태 변경
+        setDatas(newArr);
+      } catch (err) {
+        setError(err.message);
+
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchMovieData();
+  }, []);
+
+  if (loading) {
+    return <div>로딩중</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
-    <div>PopularMore</div>
-  )
+    <>
+      {datas.map((data) => {
+        const now = data.movies;
+        return now.map((item) => {
+          return <MovieCard item={item}></MovieCard>;
+        });
+      })}
+    </>
+  );
 }
