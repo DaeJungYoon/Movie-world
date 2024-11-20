d2ff9c41f1829871e2d9d4d34d04c257dd63142c
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-20 15:53:49 +0900

    [feat] NowPlayMore페이지 구현  NowPlayMore.jsx  #34

--- src/pages/NowPlayMore.jsx
+++ src/pages/NowPlayMore.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+import React from "react";
+import MovieCard from "../components/MovieCard";
+import { useState, useEffect } from "react";
+import { nowPlayingApi } from "../api/movieApi";
 
 export default function NowPlayMore() {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [datas, setDatas] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovieData() {
+      try {
+        const nowdata = await nowPlayingApi.getNowPlaying();
+
+        const newArr = [
+          {
+            kategorieTitle: "now_playing_more",
+            movies: nowdata,
+          },
+        ];
+
+        // 렌더링 할 데이터 상태 변경
+        setDatas(newArr);
+      } catch (err) {
+        setError(err.message);
+
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchMovieData();
+  }, []);
+
+  if (loading) {
+    return <div>로딩중</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
   return (
-    <div>NowPlayMore</div>
-  )
+    <>
+      {datas.map((data) => {
+        const now = data.movies;
+        return now.map((item) => {
+          return <MovieCard item={item}></MovieCard>;
+        });
+      })}
+      ;
+    </>
+  );
 }
