3402250c719bb421f7918fb55cac0ebdb8f000d5
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-20 15:48:10 +0900

    [feat, design, refactor] Kategorie별 더보기페이지로 연결되는 Link 추가, card에 flex css 추가, MovieCard에 key 추가, props 명 변경, MovieList.jsx 기능 추가, 디자인 추가, 리펙토링  #32

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import MovieCard from "./MovieCard";
+import { Link } from "react-router-dom";
 
 export default function MovieList(props) {
   const { kategorieTitle, movies } = props;
   return (
     <div>
       <h2>{kategorieTitle}</h2>
-      <p>...more</p>
-      <ul>
+
+      <p>
+        <Link to={`/kategorie/${kategorieTitle}`}>...more</Link>
+      </p>
+      <ul style={{ display: "flex" }}>
         {movies.map((item) => {
-          return <MovieCard items={item}></MovieCard>;
+          return <MovieCard key={item.id} item={item}></MovieCard>;
         })}
       </ul>
     </div>
