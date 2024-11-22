63527e19155c763127f361882cd2dc1ee50e114b
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-22 11:43:00 +0900

    [refactor] instance 하나로 합치면서  Api 재구성 movieApi.js, movieAxios.js 리펙토링 #42

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -1,32 +1,25 @@
 import {
-  nowPlayInstance,
-  popularInstance,
-  topRatedInstance,
+  instance,
+
 } from "./movieAxios";
 
 const nowPlayingApi = {
   getNowPlaying: async () => {
-    const resposne = await nowPlayInstance.get("/now_playing");
+    const resposne = await instance.get("/now_playing");
     return resposne.data.results;
   },
 };
 const popularApi = {
   getPopular: async () => {
-    const resposne = await popularInstance.get("/popular");
+    const resposne = await instance.get("/popular");
     return resposne.data.results;
   },
 };
 const topRatedApi = {
   getTopRated: async () => {
-    const resposne = await topRatedInstance.get("/top_rated");
+    const resposne = await instance.get("/top_rated");
     return resposne.data.results;
   },
 };
-const getMovieDetailApi = {
-  getMovie: async () => {
-    const resposne = await movieDetailInstance.get();
-    console.log(resposne.data);
-    return resposne.data.id;
-  },
-};
+
 export { nowPlayingApi, popularApi, topRatedApi };
--- src/api/movieAxios.js
+++ src/api/movieAxios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const nowPlayInstance = axios.create({
+const instance = axios.create({
   baseURL: import.meta.env.VITE_NOW_MOVIES,
   params: {
     api_key: import.meta.env.VITE_API_KEY,
@@ -8,49 +8,5 @@ const nowPlayInstance = axios.create({
   },
 });
 
-// baseURL: "https://api.themoviedb.org/3",
-// params: {
-//   api_key: "2211333",
-//   language: "ko-KR",
-// },
-const popularInstance = axios.create({
-  baseURL: import.meta.env.VITE_NOW_MOVIES,
-  params: {
-    api_key: import.meta.env.VITE_API_KEY,
-    language: import.meta.env.VITE_API_LANG,
-  },
-});
-const topRatedInstance = axios.create({
-  baseURL: import.meta.env.VITE_NOW_MOVIES,
-  params: {
-    api_key: import.meta.env.VITE_API_KEY,
-    language: import.meta.env.VITE_API_LANG,
-  },
-});
-// const popularInstance = axios.create({
-//   baseURL:
-//     import.meta.env.VITE_NOW_MOVIES +
-//     "/popular" +
-//     import.meta.env.VITE_API_KEY +
-//     import.meta.env.VITE_API_LANG,
-// });
-// const topRatedInstance = axios.create({
-//   baseURL:
-//     import.meta.env.VITE_NOW_MOVIES +
-//     "/top_rated" +
-//     import.meta.env.VITE_API_KEY +
-//     import.meta.env.VITE_API_LANG,
-// });
-// const movieDetailInstance = axios.create({
-//   baseURL:
-//     import.meta.env.VITE_NOW_MOVIES +
-//     `/${movie.id}` +
-//     import.meta.env.VITE_API_KEY +
-//     import.meta.env.VITE_API_LANG,
-// });
 
-// const movieDetailInstance = axios.create({
-//   baseURL: import.meta.env.VITE_NOW_MOVIES,
-// });
-
-export { nowPlayInstance, popularInstance, topRatedInstance };
+export { instance };
