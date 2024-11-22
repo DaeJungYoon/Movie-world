30a1ccd9351d55f056d1c9839af63c07daa5714e
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-22 11:35:27 +0900

    [refactor] Api 재구성 movieApi.js, movieAxios.js 리펙토링 #39

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -6,21 +6,27 @@ import {
 
 const nowPlayingApi = {
   getNowPlaying: async () => {
-    const resposne = await nowPlayInstance.get("");
+    const resposne = await nowPlayInstance.get("/now_playing");
     return resposne.data.results;
   },
 };
 const popularApi = {
   getPopular: async () => {
-    const resposne = await popularInstance.get("");
+    const resposne = await popularInstance.get("/popular");
     return resposne.data.results;
   },
 };
 const topRatedApi = {
   getTopRated: async () => {
-    const resposne = await topRatedInstance.get("");
+    const resposne = await topRatedInstance.get("/top_rated");
     return resposne.data.results;
   },
 };
-
+const getMovieDetailApi = {
+  getMovie: async () => {
+    const resposne = await movieDetailInstance.get();
+    console.log(resposne.data);
+    return resposne.data.id;
+  },
+};
 export { nowPlayingApi, popularApi, topRatedApi };
