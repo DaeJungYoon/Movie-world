4209ee16270394845c9a9c833035931b3a3c6fd8
DaeJungYoon <dbseowjd12@gmail.com>
2024-11-25 18:26:45 +0900

    [feat] MovieCard 컴포넌트에서 영화를 누르면 해당 MovieDetail로 이동할 수 있도록 Link추가  #52

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 export default function MovieCard(props) {
   // console.log(props);
   const { item } = props;
 
   return (
-    <li key={item.id}>
-      <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} />
-      <h4>{item.title}</h4>
-    </li>
+    <>
+      <Link to={`/movie/${item.id}`}>
+        <li key={item.id}>
+          <img src={`https://image.tmdb.org/t/p/w200${item.poster_path}`} />
+          <h4>{item.title}</h4>
+        </li>
+      </Link>
+    </>
   );
 }
